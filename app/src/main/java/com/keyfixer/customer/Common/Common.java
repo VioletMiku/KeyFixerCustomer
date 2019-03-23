b9bcd5a40dc7e87a71e2752cf7ba397e38bda206
VioletMiku <livuanh97@gmail.com>
2019-03-23 18:54:36 +0700

    ver 0.0.0.2, updated on 23th march 2019. Can run a map, get your location but still cann't pickup request.

---
+++ app/src/main/java/com/keyfixer/customer/Common/Common.java
@@ -0,0 +1,18 @@
+package com.keyfixer.customer.Common;
+
+import com.keyfixer.customer.Remote.IGoogleAPI;
+import com.keyfixer.customer.Remote.RetrofitClient;
+
+public class Common {
+
+    public static final String fixer_tbl = "Fixers";
+    public static final String user_fixer_tbl = "Users";
+    public static final String user_customer_tbl = "Customers";
+    public static final String fix_request_tbl = "FixRequest";
+
+    public static final String baseUrl = "https://maps.googleapis.com";
+
+    public static IGoogleAPI getGoogleAPI(){
+        return RetrofitClient.getClient(baseUrl).create(IGoogleAPI.class);
+    }
+}
